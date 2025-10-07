db3b066ed30af64f54865c89cce46886386c796c
Yan Ru Chen <163501691+Lawliet0813@users.noreply.github.com>
2025-10-07 21:13:00 +0800

    Add realtime watcher core with batching and tests

--- auto_organizer/__init__.py
+++ auto_organizer/__init__.py
@@ -1,5 +1,11 @@
 """AutoOrganizer package exports."""
 
 from .cli import main as cli_main
+from .realtime_watcher import EventType, FileSystemEvent, RealtimeWatcher
 
-__all__ = ["cli_main"]
+__all__ = [
+    "cli_main",
+    "EventType",
+    "FileSystemEvent",
+    "RealtimeWatcher",
+]
